40ec5c4cd129c066301587a41e41f84b83e45498
Cristobal Dabed <cristobal@dabed.org>
2025-07-02 21:38:39 +0200

    chore: perf (#2)
    Co-authored-by: Cristobal Dabed <cristobd@dabed.org>

--- src/classnames.ts
+++ src/classnames.ts
@@ -1,23 +1,25 @@
+/*
+  The `classNames` function is designed to be simple and have a small footprint.
+  It does some performance optimizations like:
+    1. Only mutates the `output` variable, and avoids creating additional strings.
+    2. It uses a single loop to iterate through the input classes, rather than a `for of` construct.
+    3. It uses a `for in` construct to evaluate the keys of the input objects, rather than use `Object.entries()`.
+*/
 export default function classNames(
-  ...classes: (string | Record<string, boolean> | undefined)[]
+  ...classes: (string | Record<string, boolean>)[]
 ) {
   let output = '';
-  let separator = '';
-  for (const value of classes) {
-    if (!value) {
-      continue;
+  for (let index = 0, size = classes.length; index < size; index++) {
+    // handle single string values
+    if (typeof classes[index] === 'string') {
+      output += (output && ' ') + classes[index];
     }
-
-    if (typeof value === 'string') {
-      output += separator + value;
-      separator = ' ';
-      continue;
-    }
-
-    for (const [key, predicate] of Object.entries(value)) {
-      if (predicate) {
-        output += separator + key;
-        separator = ' ';
+    // otherwise we assume it's a Record<string, boolean>
+    else {
+      for (const key in classes[index] as Record<string, boolean>) {
+        if ((classes[index] as Record<string, boolean>)[key]) {
+          output += (output && ' ') + key;
+        }
       }
     }
   }
